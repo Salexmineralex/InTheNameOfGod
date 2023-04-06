60882bc9bcaf482170a8257be5627ab002c7b22c
Alejandro Sánchez Torres <74661819+Salexmineralex@users.noreply.github.com>
2023-04-06 16:36:03 +0200

    Update TestRunnerAndCodeCoverage.bat

--- TestRunnerAndCodeCoverage.bat
+++ TestRunnerAndCodeCoverage.bat
@@ -7,6 +7,5 @@ set testsLogName=%~6
 set codeCoverageFile=%~7
 
 //
-set testRunnerCommand="%ue4Location%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%workspace%\%projectFilename%" -run=Automation -Device=Null -nullrhi -unattended -TestExit="" -ReportOutputPath="%workspace%\Saved\UnitTestsReport" -log -Log=%testsLogName% -multiprocess
-
+F:\UE_5.1\Engine\Binaries\Win64\UnrealEditor-Cmd.exe "F:\CITestingWorkspace\InTheNameOfGod.uproject" -unattended -nopause -NullRHI -ExecCmds="Automation RunTests" -testexit="Automation Test Queue Empty" -log -log=RunTests.log -ReportOutputPath="F:\CITestingWorkspace\Saved\UnitTestsReport"
 C:\OpenCppCoverage64\OpenCppCoverage\OpenCppCoverage.exe   --sources=%workspace%\Source --excluded_sources=\Tests --modules %workspace% --export_type=cobertura:%codeCoverageFile% --working_dir=F:\CITestingWorkspace\ -- %testRunnerCommand%
