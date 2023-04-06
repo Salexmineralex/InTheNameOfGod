f597c19ae7a0a8ba4374074b1b82ea69aba3590d
Salexmineralex <74661819+Salexmineralex@users.noreply.github.com>
2023-04-06 11:00:14 +0200

    Source Update

---
+++ Source/InTheNameOfGod/InTheNameOfGod.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "InTheNameOfGod.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InTheNameOfGod, "InTheNameOfGod" );
