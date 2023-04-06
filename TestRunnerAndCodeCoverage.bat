d1942e1bbfdfb580eaf8361d960423dfb01ddf48
Alejandro Sánchez Torres <74661819+Salexmineralex@users.noreply.github.com>
2023-04-06 12:27:11 +0200

    Update TestRunnerAndCodeCoverage.bat

--- TestRunnerAndCodeCoverage.bat
+++ TestRunnerAndCodeCoverage.bat
@@ -8,6 +8,8 @@ set codeCoverageFile=%~7
 
 //set testRunnerCommand="%ue4Location%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause  -NullRHI -nocontentbrowser  -ExecCmds="Automation RunTests %testSuiteToRun%;quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%workspace%\%testReportFolder%" -log -Log=%testsLogName%
 
-set testRunnerCommand="%ue4Location%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause -NullRHI -nocontentbrowser  ExecCmds="Automation RunTests;quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%workspace%\%testReportFolder%" -log -Log=%testsLogName% -WaitForExit
+//set testRunnerCommand="%ue4Location%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause -NullRHI -nocontentbrowser  ExecCmds="Automation RunTests;quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%workspace%\%testReportFolder%" -log -Log=%testsLogName% -WaitForExit
+
+set testRunnerCommand="%ue4Location%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause -NullRHI -nocontentbrowser  ExecCmds="Automation RunTests;quit" -TestExit="" -ReportOutputPath="%workspace%\%testReportFolder%" -log -Log=%testsLogName% -WaitForExit
 
 C:\OpenCppCoverage64\OpenCppCoverage\OpenCppCoverage.exe   --sources=%workspace%\Source --excluded_sources=\Tests --modules %workspace% --export_type=cobertura:%codeCoverageFile% --working_dir=F:\CITestingWorkspace\ -- %testRunnerCommand%
