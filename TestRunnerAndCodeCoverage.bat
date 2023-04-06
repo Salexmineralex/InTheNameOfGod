81cdc0e32d7d649bcaa71a4f0b46a244c688a389
Alejandro Sánchez Torres <74661819+Salexmineralex@users.noreply.github.com>
2023-04-06 13:57:09 +0200

    Update TestRunnerAndCodeCoverage.bat

--- TestRunnerAndCodeCoverage.bat
+++ TestRunnerAndCodeCoverage.bat
@@ -16,6 +16,6 @@ set codeCoverageFile=%~7
 
 //set testRunnerCommand="%ue4Location%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause -nocontentbrowser -TestExit="Automation Test Queue Empty" -ReportOutputPath="%workspace%\%testReportFolder%" -log -Log=%testsLogName% -WaitForExit
 
-set testRunnerCommand="%ue4Location%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%workspace%\%projectFilename%" -run=Automation -Device=Null -nullrhi -unattended -testexit="Automation Test Queue Empty" -reportoutputpath="%workspace%\%testReportFolder%" -log -logfilename=%testsLogName% -multiprocess -WaitForExit
+set testRunnerCommand="%ue4Location%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%workspace%\%projectFilename%" -run=Automation -Device=Null -nullrhi -unattended -testexit="" -reportoutputpath="%workspace%\%testReportFolder%" -log -logfilename=%testsLogName% -multiprocess -WaitForExit
 
 C:\OpenCppCoverage64\OpenCppCoverage\OpenCppCoverage.exe   --sources=%workspace%\Source --excluded_sources=\Tests --modules %workspace% --export_type=cobertura:%codeCoverageFile% --working_dir=F:\CITestingWorkspace\ -- %testRunnerCommand%
